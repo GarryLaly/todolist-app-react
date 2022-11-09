9633a07bae718c5acf3552e45d3e8094629e4f6b
GarryLaly <garrylaly@gmail.com>
2022-11-09 10:41:50 +0700

    Todo list simple app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState({
+    random: Math.random(),
+    data: [
+      { title: "Baca buku" },
+      { title: "Olahraga" },
+      { title: "Istirahat" },
+    ],
+  });
+  const [newTodo, setNewTodo] = useState("");
+
+  const actionAddTodo = () => {
+    const newData = list.data;
+    newData.push({ title: newTodo });
+    setList({ random: Math.random(), data: newData });
+    setNewTodo("");
+  };
+
+  const actionDeleteTodo = (index) => {
+    const newData = list.data;
+    newData.splice(index, 1);
+    setList({ random: Math.random(), data: newData });
+    setNewTodo("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ maxWidth: 300, margin: "auto" }}>
+        <h1>Todo List App</h1>
+        <br />
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="Ketik todo disini..."
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <Button variant="primary" onClick={actionAddTodo}>
+            Tambahkan
+          </Button>
+        </InputGroup>
+        <ListGroup as="ol" numbered>
+          {list.data.map((item, index) => (
+            <ListGroup.Item
+              key={index}
+              as="li"
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              {item.title}
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={() => actionDeleteTodo(index)}
+              >
+                Hapus
+              </Button>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
     </div>
   );
 }
